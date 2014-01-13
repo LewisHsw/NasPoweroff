06be64dfd858ba0a1bd22026a4e5333bfa7d3283
oscar.riva <sezione9@gmail.com>
2014-01-13 12:00:49 +0100

    added navigation drawer

--- NasPoweroff/gen/com/figura4/naspoweroff/R.java
+++ NasPoweroff/gen/com/figura4/naspoweroff/R.java
@@ -8,6 +8,9 @@
 package com.figura4.naspoweroff;
 
 public final class R {
+    public static final class array {
+        public static final int planets_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,47 +20,65 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int action_search=0x7f020000;
+        public static final int drawer_shadow=0x7f020001;
+        public static final int earth=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int jupiter=0x7f020005;
+        public static final int mars=0x7f020006;
+        public static final int mercury=0x7f020007;
+        public static final int neptune=0x7f020008;
+        public static final int saturn=0x7f020009;
+        public static final int uranus=0x7f02000a;
+        public static final int venus=0x7f02000b;
     }
     public static final class id {
-        public static final int action_exit=0x7f090003;
-        public static final int action_settings=0x7f090002;
-        public static final int editConsole=0x7f090001;
-        public static final int poweroff_button=0x7f090000;
+        public static final int action_exit=0x7f0a0005;
+        public static final int action_settings=0x7f0a0004;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int editConsole=0x7f0a0002;
+        public static final int left_drawer=0x7f0a0003;
+        public static final int poweroff_button=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int drawer_list_item=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_exit=0x7f060003;
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int message_command_sent=0x7f060006;
-        public static final int message_connect=0x7f060008;
-        public static final int message_connecting=0x7f060005;
-        public static final int message_sending_command=0x7f060009;
-        public static final int message_setup_connection=0x7f060007;
-        public static final int poweroff_button=0x7f060004;
-        public static final int pref_advanced_title=0x7f060010;
-        public static final int pref_nas_command_default=0x7f060015;
-        public static final int pref_nas_command_title=0x7f06000e;
-        public static final int pref_nas_ip_address_default=0x7f060011;
-        public static final int pref_nas_ip_address_title=0x7f06000a;
-        public static final int pref_nas_ssh_password_default=0x7f060013;
-        public static final int pref_nas_ssh_password_title=0x7f06000c;
-        public static final int pref_nas_ssh_port_default=0x7f060014;
-        public static final int pref_nas_ssh_port_title=0x7f06000d;
-        public static final int pref_nas_ssh_username_default=0x7f060012;
-        public static final int pref_nas_ssh_username_title=0x7f06000b;
-        public static final int pref_network_title=0x7f06000f;
-        public static final int title_activity_settings=0x7f060001;
+        public static final int action_exit=0x7f060007;
+        public static final int action_settings=0x7f060006;
+        public static final int action_websearch=0x7f060002;
+        public static final int app_name=0x7f060004;
+        public static final int app_not_available=0x7f060003;
+        public static final int drawer_close=0x7f060001;
+        public static final int drawer_open=0x7f060000;
+        public static final int message_command_sent=0x7f06000a;
+        public static final int message_connect=0x7f06000c;
+        public static final int message_connecting=0x7f060009;
+        public static final int message_sending_command=0x7f06000d;
+        public static final int message_setup_connection=0x7f06000b;
+        public static final int poweroff_button=0x7f060008;
+        public static final int pref_advanced_title=0x7f060014;
+        public static final int pref_nas_command_default=0x7f060019;
+        public static final int pref_nas_command_title=0x7f060012;
+        public static final int pref_nas_ip_address_default=0x7f060015;
+        public static final int pref_nas_ip_address_title=0x7f06000e;
+        public static final int pref_nas_ssh_password_default=0x7f060017;
+        public static final int pref_nas_ssh_password_title=0x7f060010;
+        public static final int pref_nas_ssh_port_default=0x7f060018;
+        public static final int pref_nas_ssh_port_title=0x7f060011;
+        public static final int pref_nas_ssh_username_default=0x7f060016;
+        public static final int pref_nas_ssh_username_title=0x7f06000f;
+        public static final int pref_network_title=0x7f060013;
+        public static final int title_activity_settings=0x7f060005;
     }
     public static final class style {
         /** 
@@ -81,11 +102,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
